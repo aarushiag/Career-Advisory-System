af1b3016968af1f1b658fb46dba21297cb961e68
Aarushi Agarwal <aarushi16216@iiitd.ac.in>
2020-09-24 00:29:00 +0530

    Added grade and review test conditions.

--- assignment1.pl
+++ assignment1.pl
@@ -1,57 +1,48 @@
-/*-----------------------------------------------CAREER ADVISORY SYSTEM------------------------------------------------------------*/
-
-/*------------------------------------------------ EXPLANATION ------------------------------------------------------------*/
-
-explanation(masters) :- write('---------------------------------------- \n
-The suggested career for you is masters \n----------------------------------------').
-
-explanation(mtech) :- write('---------------------------------------- \n
-The suggested career for you is mtech \n----------------------------------------').
-
-explanation(mba) :- write('---------------------------------------- \n 
-The suggested career for you is mba \n----------------------------------------').
-
-explanation(start-up) :- write('------------------------------------------------------------\n 
-The suggested career for you is Entrepreneurship(start-up)\n------------------------------------------------------------').
-
-explanation(navy) :- write('---------------------------------------- \n 
-The suggested career for you is Indian Armed Forces\n----------------------------------------').
-
-explanation(civil) :- write('---------------------------------------- \n 
-The suggested career for you is Civil Services\n----------------------------------------').
-
-explanation(navyAndCivil) :- write('---------------------------------------- \n 
-The suggested careers for you are Indian Armed Forces and Civil Services\n----------------------------------------').
-
-explanation(psu) :- write('---------------------------------------- \n 
-The suggested career for you is Pubilc Services Undertaking\n----------------------------------------').
-
-explanation(arts) :- write('---------------------------------------- \n 
-The suggested career for you is Artist\n----------------------------------------').
-
-explanation(data_analyst) :- write('----------------------------------------------------- 
-The suggested career for you is Data Analyst\n-----------------------------------------------------\n').
-
-explanation(big_data_eng) :- write('----------------------------------------------------- \n
-The suggested career for you is Big Data Engineer\n-----------------------------------------------------\n').
-
-explanation(dataScientist) :- write('----------------------------------------------------- \n
-The suggested career for you is Data Scientist\n-----------------------------------------------------\n').
-
-explanation(full) :- write('----------------------------------------------------- \n
-The suggested career for you is Full Stack Developer\n-----------------------------------------------------\n').
-
-explanation(sde) :- write('----------------------------------------------------- \n
-The suggested career for you is Software Developer\n-----------------------------------------------------\n').
-
-explanation(blockchain) :- write('----------------------------------------------------- \n
-The suggested career for you is Blockchain Developer\n-----------------------------------------------------\n').
- 
- 
-/*------------------------------------------------ CORPORATE ------------------------------------------------------------*/
+/*-----------------------------------------------CAREER ADVISORY SYSTEM ------------------------------------------------------------*/
 
 :- discontiguous career/1.
 :- discontiguous career/2.
+
+/*=================================================== MAIN PROGRAM ================================================================*/
+
+advise_career(Branch, CGPA) :- retractall(interest(_,_)), retractall(grade(_,_)), retractall(branch(_)), retractall(cgpa(_)), retractall(recommended(_)), 
+assert(branch(Branch)), assert(cgpa(CGPA)), initial_review_test, suggest_career.
+
+initial_review_test :- write('\nPlease give the following review test :-'), nl, ask_interest('Ability to communicate'), ask_interest('Presentation skills'),
+ask_interest('Knowledge of core subjects'),ask_interest('Critical Thinking and Problem Solving'), ask_interest('Desire to Learn'), 
+ask_interest('Teamwork'), ask_interest('Attention to details'), ask_interest('Creativity'), ask_interest('Efficient in Time Management').
+
+suggest_career :- nl,write('\nWelcome to the career advisory system ! \n
+Choose a preference from below :- \n 1. Want to study further \n 2. Want to do something on your own?\n 3. Want to serve nation?\n 4. Have a side passion?\n 5. Prefer corporate culture?\n 6. Show final suggestions?'),
+nl,(read(Y), nl, ((Y =:= 1 -> (category(higher_studies))); (Y =:= 2 -> career(start_up)) ; (Y =:= 3 -> career(government_job))
+; (Y =:= 4 -> career(arts)) ; (Y =:= 5 -> category(corporate)) ; (Y =:= 6 -> final_suggestion) ; write('Please choose the correct option \n'),suggest_career)).
+
+final_suggestion :- nl, convert_to_list(List), remove_duplicates(List, Final_List),
+write('The list of suggested career choices along with their priority suited for you are :- \n'), write(Final_List).
+
+convert_to_list([Career|Tail]):- retract(recommended(Career)), convert_to_list(Tail).
+convert_to_list([]).
+
+remove_duplicates([], []).
+remove_duplicates([Head | Tail], Result) :- member(Head, Tail), remove_duplicates(Tail,Result), !.
+remove_duplicates([Head | Tail], [Head | Result]) :- remove_duplicates(Tail, Result), !.
+
+ask_interest(Topic):- write('\nEnter your level of expertise for :- '), write(Topic), nl, nl,
+write('0 - Absent\n1 - Low Expertise\n2 - Medium Expertise\n3 - High Expertise\n'), nl, read(Level), assert(interest(Topic, Level)).
+
+ask_grade(Course):- write('\nEnter your grade (on a scale of 10) in the course :- '), write(Course), nl, 
+					write('NOTE :- Enter 0 if the course is not done'), nl,
+					read(Grade), assert(grade(Course, Grade)).
+					
+check_interest_threshold(Interest, Threshold) :- interest(Interest, Level), Level >= Threshold. 
+check_grade_threshold([], _Threshold) :- fail.  
+check_grade_threshold([H|T], Threshold) :- ((grade(H, Grade), Grade >= Threshold) ; check_grade_threshold(T, Threshold)),!.
+
+insert(X, L, [X|L]).
+ 
+/*------------------------------------------------ CORPORATE ------------------------------------------------------------*/
+
+
 
 suggestCSE :- write('Choose a preference from below :- \n
 1. Worked with analyzing large amount of data and extracting hidden patterns.
@@ -98,14 +89,12 @@ write('Are you aware of technologies like Bitcoin?'), nl, read(A), nl, A=y,
 write('Have you done courses like Probability and Statistics, Discrete Mathematics, Cryptography, Bitcoin etc?'), nl, read(B), nl, B=y, explanation(blockchain)
 ,nl, assertz(recommended('Blockchain Developer')),fail.
 
-category(corporate) :- (write('Mention your branch in small letters.'), nl, 
-(read(B), nl, (B = cse -> (suggestCSE) ; B= ece -> (suggestECE) ; B= csam -> (suggestCSAM); 
-(write('Please mention a valid branch\n'),category(corporate)))),suggest_career);suggest_career.
+category(corporate) :- (branch(B),(B = cse -> (suggestCSE) ; B = ece -> (suggestECE) ; B = csam -> (suggestCSAM); 
+(write('Please mention a valid branch\n'),category(corporate))),suggest_career);suggest_career.
 
 
 /*---------------------------------------------HIGHER STUDIES------------------------------------------------------------*/
-category(higher_studies) :- (write('Do you have an economic background?'), nl, read(E), nl, E=y,
-write('Can you study for long hours?'), nl, read(L), nl, L=y, 
+category(higher_studies) :- (write('Can you study for long hours?'), nl, read(L), nl, L=y, 
 write('Do you want to do specialization in core subjects?'), nl, read(Y), nl, 
 (Y=y -> ask_location_pref ; Y=n -> career(mba,higher_studies) ; write('Please choose the correct option \n'),category(higher_studies)));suggest_career.
 
@@ -114,28 +103,41 @@ ask_location_pref :- (write('Choose a preference from below :- \n 1. Want to stu
 career(mtech,higher_studies))); write('Please choose the correct option \n'), ask_location_pref))).
 
 /*---------------------- MTECH ---------------------------------*/
-career(mtech,higher_studies) :-  write('Do you like research?'), nl, read(R), nl, R=y, write('Good Score in GATE?'), nl,read(G), nl, G=y,
-write('Type your CGPA on scale of 10'), nl,read(C), nl, C >= 8, explanation(mtech),nl, assertz(recommended('mtech')), fail.
-
+career(mtech,higher_studies) :-  write('Do you like research?'), nl, read(R), nl, R=y, 
+check_interest_threshold('Knowledge of core subjects',3),
+check_interest_threshold('Teamwork',2),
+check_interest_threshold('Critical Thinking and Problem Solving',2),
+check_interest_threshold('Desire to Learn',2),
+write('Have you worked on a research problem (in BTP/Course Projects)?'), nl,read(G), nl, G=y,
+cgpa(Grade), Grade >= 7, explanation(mtech),nl, assertz(recommended('mtech')), fail.
 
 /*---------------------- MASTERS ---------------------------------*/
-career(masters,higher_studies) :-  write('Do you like research?'), nl,read(R), nl, R=y, write('Good Score in GRE?'), read(G), nl, G=y,
-write('Good Score in TOEFL?'), read(T), nl, T=y, write('Type your CGPA on scale of 10'), read(C), nl, C >= 8, 
-explanation(masters),nl, assertz(recommended('masters')), fail.
-
+career(masters,higher_studies) :-  write('Do you have an economic background?'), nl, read(E), nl, E=y,
+write('Do you like research?'), nl, read(R), nl, R=y, 
+check_interest_threshold('Teamwork',2),
+check_interest_threshold('Critical Thinking and Problem Solving',2),
+check_interest_threshold('Desire to Learn',2),
+write('Have you worked on a research problem (in BTP/Course Projects)?'), nl,read(G), nl, G=y,
+cgpa(Grade), Grade >= 7, explanation(masters),nl, assertz(recommended('masters')), fail.
 
 /*---------------------- MBA ---------------------------------*/
 career(mba,higher_studies) :- write('Are you good in extra curicular activities?'), nl,read(R), nl, R=y, 
-write('Do you have excellent communication skills?'), nl,read(C), nl, C=y, 
-write('Have you managed events at esya or odessey?'), nl,read(M), nl, M=y, write('Do you prefer being a leader in group projects?'), 
-nl,read(P), nl, P=y, write('Did you get a good score in CAT/GMAT?'), 
-nl,read(S), nl, S=y,explanation(mba),nl, assertz(recommended('mba')), fail. 
+check_interest_threshold('Ability to communicate',3), check_interest_threshold('Presentation skills',2), check_interest_threshold('Critical Thinking and Problem Solving',2),
+write('Have you managed events at esya or odessey?'), nl,read(M), nl, M=y, 
+write('Do you prefer being a leader in group projects?'), nl,read(P), nl, P=y, 
+ask_grade(eiitm), ask_grade(escm),ask_grade(hiee), ask_grade(ek), ask_grade(eComm), check_grade_threshold([eiitm, escm, hiee, ek, eComm],9),
+explanation(mba),nl, assertz(recommended('mba')), fail. 
 
-/*---------------------- START UP-----------------------------*/
-career(start_up) :- (write('Do you have a new development idea?'), nl,read(D), nl, D=y, write('Do you have money to invest?'), nl,read(M), nl, M=y, 
-write('Are you willing to take monetary risk in life?'), nl,read(R), nl, R=y, explanation(start-up),nl, assertz(recommended('Entrepreneurship(start-up)')),
+/*------------------------------------------------- START UP----------------------------------------------------------------*/
+career(start_up) :- (write('Do you have a new development idea?'), nl,read(D), nl, D=y, 
+write('Do you have money to invest?'), nl,read(M), nl, M=y, 
+write('Are you willing to take monetary risk in life?'), nl,read(R), nl, R=y, 
+check_interest_threshold('Ability to communicate',3), check_interest_threshold('Presentation skills',2), check_interest_threshold('Teamwork',3),check_interest_threshold('Efficient in Time Management',2),
+ask_grade(eiitm), ask_grade(escm),ask_grade(hiee), ask_grade(ek), ask_grade(eComm), check_grade_threshold([eiitm, escm, hiee, ek, eComm],9),
+explanation(start-up),nl, assertz(recommended('Entrepreneurship(start-up)')),
 suggest_career); suggest_career.
-/*---------------------- GOVERNMENT JOB --------------------*/
+
+/*----------------------------------------------- GOVERNMENT JOB -----------------------------------------------------------*/
 career(government_job) :- (write('Choose a preference from below :- \n 1. Power matters more than money?\n 2. Want a good work life balance'), nl, read(Y), nl, 
 (Y=1 -> career(navyOrCivil), suggest_career ; Y=2 -> career(psu) ; write('Please choose the correct option \n'),career(government_job)));suggest_career.
 
@@ -154,7 +156,7 @@ career(psu) :- (write('Are you concerned about job security?'), nl,read(J), nl, 
 write('Do you have a good score in GATE?'), nl,read(G), nl, G=y, explanation(psu),nl, assertz(recommended('Pubilc Services Undertaking')),
 suggest_career); suggest_career.
 
-/*---------------------- ARTS --------------------*/
+/*---------------------------------------------------- ARTS ---------------------------------------------------------------*/
 
 career(arts) :- (write('Are you willing to take risk?'), nl,read(R), nl, R=y , 
 write('Do you have proficiency in Photography/Dance/Music/Writing?'), nl,read(H), nl, H=y ,
@@ -162,23 +164,60 @@ write('Do you have proficiency in Photography/Dance/Music/Writing?'), nl,read(H)
 write('Do you participate in competitions like reality shows or on Youtube?'), nl,read(Y), nl, Y=y),
 explanation(arts),nl, assertz(recommended('Artist')),
 suggest_career);suggest_career.
- 
-/*=================================================== MAIN PROGRAM ================================================================*/
 
-suggest_career :- nl,write('\nWelcome to the career advisory system ! \n
-Choose a preference from below :- \n 1. Want to study further \n 2. Want to do something on your own?\n 3. Want to serve nation?\n 4. Have a side passion?\n 5. Prefer corporate culture?\n 6. Show final suggestions?'),
-nl,(read(Y), nl, ((Y =:= 1 -> (category(higher_studies))); (Y =:= 2 -> career(start_up)) ; (Y =:= 3 -> career(government_job))
-; (Y =:= 4 -> career(arts)) ; (Y =:= 5 -> category(corporate)) ; (Y =:= 6 -> final_suggestion) ; write('Please choose the correct option \n'),suggest_career)).
+/*------------------------------------------------ EXPLANATION -----------------------------------------------------------*/
 
-final_suggestion :- nl, convert_to_list(List), remove_duplicates(List, Final_List), length(Final_List, Length), prioritize(Final_List, Final_PList, Length), 
-write('The list of suggested career choices along with their priority suited for you are :- \n'), write(Final_PList).
+explanation(masters) :- write('------------------------------------------------------------------------
+The suggested career for you is masters.\n
+Explanation - You are an efficient Team player, critical thinker and have a keen desire to learn. You have also find some novel solutions to problems. Start preparing for TOEFL and GRE exam.
+------------------------------------------------------------------------').
 
-convert_to_list([Career|Tail]):- retract(recommended(Career)), convert_to_list(Tail).
-convert_to_list([]).
+explanation(mtech) :- write('------------------------------------------------------------------------
+The suggested career for you is mtech \n
+Explanation - You are proficient in Core subjects. You are an efficient Team player, critical thinker and have a keen desire to learn. You have also find some novel solutions to problems. Start preparing for GATE exam.
+------------------------------------------------------------------------').
 
-remove_duplicates([], []).
-remove_duplicates([Head | Tail], Result) :- member(Head, Tail), remove_duplicates(Tail,Result), !.
-remove_duplicates([Head | Tail], [Head | Result]) :- remove_duplicates(Tail, Result), !.
+explanation(mba) :- write('------------------------------------------------------------------------
+The suggested career for you is mba.\n
+Explanation - You have good presentation and communication skills and are good at managing public groups events. You are a critical thinker. Start preparing for CAT exam.
+------------------------------------------------------------------------').
 
-prioritize([], [], _Total_length).
-prioritize([H|T], [H1|Result], Total_length) :- prioritize(T, Result, Total_length), length(T, TailLength), X is Total_length - TailLength, H1 = (H , X).
+explanation(start-up) :- write('------------------------------------------------------------------------
+The suggested career for you is Entrepreneurship(start-up).
+Explanation - You are a good team player, can think critically and have good management skills.Hence, you can start a new venture. 
+------------------------------------------------------------------------').
+
+explanation(navy) :- write('---------------------------------------- \n 
+The suggested career for you is Indian Armed Forces\n----------------------------------------').
+
+explanation(civil) :- write('---------------------------------------- \n 
+The suggested career for you is Civil Services\n----------------------------------------').
+
+explanation(navyAndCivil) :- write('---------------------------------------- \n 
+The suggested careers for you are Indian Armed Forces and Civil Services\n----------------------------------------').
+
+explanation(psu) :- write('---------------------------------------- \n 
+The suggested career for you is Pubilc Services Undertaking\n----------------------------------------').
+
+explanation(arts) :- write('---------------------------------------- \n 
+The suggested career for you is Artist\n----------------------------------------').
+
+explanation(data_analyst) :- write('----------------------------------------------------- 
+The suggested career for you is Data Analyst\n-----------------------------------------------------\n').
+
+explanation(big_data_eng) :- write('----------------------------------------------------- \n
+The suggested career for you is Big Data Engineer\n-----------------------------------------------------\n').
+
+explanation(dataScientist) :- write('----------------------------------------------------- \n
+The suggested career for you is Data Scientist\n-----------------------------------------------------\n').
+
+explanation(full) :- write('----------------------------------------------------- \n
+The suggested career for you is Full Stack Developer\n-----------------------------------------------------\n').
+
+explanation(sde) :- write('----------------------------------------------------- \n
+The suggested career for you is Software Developer\n-----------------------------------------------------\n').
+
+explanation(blockchain) :- write('----------------------------------------------------- \n
+The suggested career for you is Blockchain Developer\n-----------------------------------------------------\n').
+
+/*-----------------------------------------------( made by - Aarushi Agarwal (2016216)) ------------------------------------------------------------*/
